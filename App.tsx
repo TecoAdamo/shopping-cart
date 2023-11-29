c747711023c9f96e6939cae99ca5c6eb93999644
Mateus Adamo <adamomateus478@gmail.com>
2023-11-29 15:31:52 -0300

    feat: ajusted screens with navigations

--- App.tsx
+++ App.tsx
@@ -1,10 +1,46 @@
 import React from 'react';
-import Home from './src/home/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './src/Screens/Home';
+import Cart from './src/Screens/Cart'
 
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <Home />
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerTintColor: 'white',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+
+      >
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerShown: false
+          }}
+        />
+
+        <Stack.Screen
+          name="Cart"
+          component={Cart}
+
+          options={{
+            cardStyle: { backgroundColor: "#343541" },
+            headerStyle: { backgroundColor: "#343541" },
+            headerTransparent: true,
+            headerShadowVisible: false,
+
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
