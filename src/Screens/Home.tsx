0bc161838ee57c988f54c3f51709167a0aa30799
Mateus Adamo <adamomateus478@gmail.com>
2023-11-29 15:34:36 -0300

    feat: shopping cart screen created

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -1,13 +1,25 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
+import { useNavigation } from "@react-navigation/native";
+
+type NavProps = {
+    navigate: (value: string) => void;
+}
+
 export default function Home() {
+    const navigation = useNavigation<NavProps>();
+
+    const navigateToCartScreen = () => {
+        navigation.navigate('Cart');
+    };
+
     return (
         <View style={styles.container}>
 
             <View style={styles.boxText}>
                 <Text style={styles.textHeader}>Lista de produtos</Text>
-                <TouchableOpacity style={styles.btnIcon}>
+                <TouchableOpacity style={styles.btnIcon} onPress={navigateToCartScreen}>
                     <Ionicons name="cart-outline" color={"white"} size={30} />
                 </TouchableOpacity>
             </View>
@@ -24,7 +36,7 @@ const styles = StyleSheet.create({
         justifyContent: "center",
     },
     boxText: {
-        top: 40,
+        top: 50,
         width: '100%',
         alignItems: 'center',
         flexDirection: 'row',
